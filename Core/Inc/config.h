20636ede40d27b9823dee78abe691f00d589b2b4
mmachac <37183472+mmachac@users.noreply.github.com>
2024-07-03 11:44:59 +0200

    Update config.h2

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -4,8 +4,8 @@
 #define BRAKEMAX 100
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
+#define SPEEDLIMIT_ECO 20
+#define SPEEDLIMIT_NORMAL 25
 #define SPEEDLIMIT_SPORT 50
 
 // motor current limits for invividual modes in mA
@@ -15,10 +15,10 @@
 #define PH_CURRENT_MAX_SPORT 25000
 
 // motor current limit for regen in mA
-#define REGEN_MAX_CURRENT 10000
+#define REGEN_MAX_CURRENT 18000
 
 // maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 0 //max id
+#define FIELD_WEAKNING_CURRENT_MAX 1 //max id
 
 // ADC channels
 #define ADC_VOLTAGE 0
